1cf64e2f3d4649de9e0c693aa4b385372ee4616c
Jessica Fuller <jessicanfuller808@gmail.com>
2022-02-28 21:58:24 -0800

    added the rest of the questions

--- script.js
+++ script.js
@@ -116,3 +116,17 @@ function newTimesTable() {
 }
 
 // newTimesTable();
+
+//---------------------------------------------
+// == Neither Yes nor No ==
+// Write a program that plays "neither yes, nor no" with the user. Specifically, the programs asks the user to enter text until either "yes" or "no" is typed, which ends the game.
+
+//---------------------------------------------
+// == Fizz Buzz ==
+// Write a program that shows all numbers between 1 and 100 with the following exceptions:
+
+// It shows "Fizz" instead if the number is divisible by 3.
+
+// It shows "Buzz" instead if the number is divisible by 5 and not by 3.
+
+// When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
